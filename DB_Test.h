6cf0904ef1c470d8e3e948a15bb356845dc507ac
dbourne <db@davids-iMac.local>
2014-04-24 19:55:28 -0600

    added to our test class

---
+++ DB_Test.h
@@ -0,0 +1,13 @@
+//
+//  DB_Test.h
+//  Git Test
+//
+//  Created by dbourne on 4/24/14.
+//  Copyright (c) 2014 David Bourne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DB_Test : NSObject
+
+@end
---
+++ DB_Test.m
@@ -0,0 +1,13 @@
+//
+//  DB_Test.m
+//  Git Test
+//
+//  Created by dbourne on 4/24/14.
+//  Copyright (c) 2014 David Bourne. All rights reserved.
+//
+
+#import "DB_Test.h"
+
+@implementation DB_Test
+
+@end
